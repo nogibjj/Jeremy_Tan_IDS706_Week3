899af997605050aff8483a051d47f834ebe84ff4
jeremymtan <jeremytan@Jeremys-MacBook-Pro.local>
2023-09-12 12:59:17 -0400

    finished polars

--- main.py
+++ main.py
@@ -1,19 +1,56 @@
-"""
-Main cli or app entry point
-"""
-
-from mylib.calculator import add
-import click
-
-#var=1;var=2
-
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
+import pandas as pd
+import matplotlib.pyplot as plt
+from ydata_profiling import ProfileReport
+import polars as pl
 
 
-if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+def general_polars_describe(csv):
+    """polars describe function in csv"""
+    polars_df = pl.read_csv(csv)
+    return polars_df.median(), polars_df.describe()
+
+
+def general_describe(csv):
+    """general describe function in csv"""
+    general_df = pd.read_csv(csv)
+    return general_df.describe()
+
+
+def general_polars_summary_with_profile(csv):
+    """general describe function in csv"""
+    polars_df = pl.scan_csv(csv)
+    profile = ProfileReport(polars_df, title="Profiling Report")
+    profile.to_file("polars.html")
+
+
+def generate_vis_general_polars_congress(csv):
+    """generate example visualization of the congress dataset"""
+    pd.set_option("display.max_columns", None)
+    polars_df = pl.read_csv(csv)
+    plt.figure(figsize=(10, 6))
+    plt.hist(polars_df["age"], bins=20, edgecolor="black")
+    plt.title("Age Distribution of Congress Members")
+    plt.xlabel("Age")
+    plt.ylabel("Frequency")
+    plt.show()
+
+
+def generate_vis_general_congress(csv):
+    """generate example visualization of the congress dataset"""
+    pd.set_option("display.max_columns", None)
+    general_df = pd.read_csv(csv)
+    # print(general_df.head())
+    # print(general_df.describe())
+    plt.figure(figsize=(10, 6))
+    plt.hist(general_df["age"], bins=20, edgecolor="black")
+    plt.title("Age Distribution of Congress Members")
+    plt.xlabel("Age")
+    plt.ylabel("Frequency")
+    plt.show()
+
+
+def generate_summary(csv):
+    """generates report of any dataset"""
+    general_df = pd.read_csv(csv)
+    profile = ProfileReport(general_df, title="Profiling Report")
+    profile.to_file("profile.html")
--- test_main.py
+++ test_main.py
@@ -3,8 +3,43 @@ Test goes here
 
 """
 
-from mylib.calculator import add
+from main import (
+    generate_vis_general_polars_congress,
+    generate_summary,
+    general_polars_describe,
+)
+
+example_csv = "https://raw.githubusercontent.com/fivethirtyeight/data/master/congress-age/congress-terms.csv"
 
 
-def test_add():
-    assert add(1, 2) == 3
+def test_general_describe():
+    """Function calling describe_iris and general_describe which tests different parts of
+    the dataset"""
+
+    # only works for the example_csv link
+    median_test, desribe_test = general_polars_describe(example_csv)
+
+    # write an assert statement to check median_test and desribe_test of age column
+    # mean of age column in congress dataset
+    assert median_test["age"][0] == 53.0
+    # standard deviation of age column in congress dataset
+    assert desribe_test[["describe", "age"]][2, 1] == 53.31373222430909
+    # mean of age column in congress dataset
+    assert desribe_test[["describe", "age"]][3, 1] == 10.678469157541093
+
+
+def test_viz_general():
+    """Function calling generate_vis_general_polars_congress"""
+
+    generate_vis_general_polars_congress(example_csv)
+
+
+def test_generate_summary_report():
+    """Function calling generate_summary()"""
+    generate_summary(example_csv)
+
+
+if __name__ == "__main__":
+    test_general_describe()
+    # test_viz_general()
+    # test_generate_summary_report()
